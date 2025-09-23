1bcf6c3d9d1164a51a5d28992a86ab5674b4962a
imahsanvs <imahsan600@gmail.com>
2025-09-23 23:36:31 +0500

    Designed Landing Page

--- app/SignUp.tsx
+++ app/SignUp.tsx
@@ -1,16 +1,30 @@
+import { theme } from "@/theme/colorsThemes";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
+import { widthPercentageToDP as wp } from "react-native-responsive-screen";
+import { SafeAreaView } from "react-native-safe-area-context";
 
+//TODO: Complete the SignUp
 export default function SignUp() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-red-950">
+      <View className="flex-1">
+        {/* Background section */}
+        <View className="bg-blue-950 items-center">
+          <Image source={require("../assets/images/backgroundImage.png")} />
+        </View>
+        <View
+          className="flex-1"
+          style={{
+            backgroundColor: theme.colors.background(1),
+            paddingHorizontal: wp(4),
+            borderTopEndRadius: wp(2),
+            borderTopStartRadius: wp(2),
+          }}
+        >
+          <Text>Hi</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,18 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import React from "react";
+
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    "roboto-bold": require("../assets/fonts/Roboto_Condensed-Bold.ttf"),
+    "roboto-regular": require("../assets/fonts/Roboto_Condensed-Regular.ttf"),
+  });
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="Landing" options={{ headerShown: false }} />
-      <Stack.Screen name="SignUp" options={{ headerShown: false }} />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="Landing" />
+      <Stack.Screen name="SignUp" />
+      <Stack.Screen name="(tabs)" />
     </Stack>
   );
 }
