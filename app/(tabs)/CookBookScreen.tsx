57c97bb9965596001a4af919f43fcf06d972eb48
imahsanvs <imahsan600@gmail.com>
2025-09-30 16:52:49 +0500

    Implemented Tabs

---
+++ app/(tabs)/CookBookScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function CookBookScreen() {
+  return (
+    <View>
+      <Text>CookBookScreen</Text>
+    </View>
+  );
+}
