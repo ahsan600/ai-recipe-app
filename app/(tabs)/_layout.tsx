3729478639784a923a47667d52b751553ec3e190
imahsanvs <imahsan600@gmail.com>
2025-09-30 16:52:49 +0500

    Implemented Tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,105 @@
 // tabs layout
 import ProtectRoute from "@/components/Auth/ProtectRoute";
+import { theme } from "@/theme/colorsThemes";
+import { regularFont } from "@/theme/fontTheme";
 import { Tabs } from "expo-router";
 import React from "react";
+import { Image } from "react-native";
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from "react-native-responsive-screen";
 
 export default function TabsLayout() {
   return (
     <ProtectRoute>
-      <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarActiveTintColor: theme.colors.background(1),
+          tabBarStyle: {
+            padding: hp(10),
+          },
+          tabBarLabelStyle: {
+            fontSize: wp(regularFont),
+            fontWeight: "600",
+            marginTop: hp(0.3),
+          },
+          tabBarItemStyle: {
+            paddingVertical: hp(0.5),
+          },
+        }}
+      >
         <Tabs.Screen
           name="HomeScreen"
           options={{
             title: "Home",
+            tabBarLabelStyle: {
+              fontSize: wp(regularFont),
+            },
+            tabBarIcon: ({ color }) => (
+              <Image
+                source={require("@/assets/images/homeIcon.png")}
+                style={{
+                  width: hp(5),
+                  height: hp(5),
+                }}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="ExploreScreen"
+          options={{
+            title: "Explore",
+            tabBarLabelStyle: {
+              fontSize: wp(regularFont),
+            },
+            tabBarIcon: ({ color }) => (
+              <Image
+                source={require("@/assets/images/dishIcon.png")}
+                style={{
+                  width: hp(5),
+                  height: hp(5),
+                }}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="CookBookScreen"
+          options={{
+            title: "CookBook",
+            tabBarLabelStyle: {
+              fontSize: wp(regularFont),
+            },
+            tabBarIcon: ({ color }) => (
+              <Image
+                source={require("@/assets/images/bookIcon.png")}
+                style={{
+                  width: hp(5),
+                  height: hp(5),
+                }}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="ProfileScreen"
+          options={{
+            title: "Profile",
+            tabBarLabelStyle: {
+              fontSize: wp(regularFont),
+            },
+            tabBarIcon: ({ color }) => (
+              <Image
+                source={require("@/assets/images/personIcon.png")}
+                style={{
+                  width: hp(5),
+                  height: hp(5),
+                }}
+              />
+            ),
           }}
         />
       </Tabs>
